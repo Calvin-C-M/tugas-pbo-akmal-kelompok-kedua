2c9399f4bbf114b0022f05dd7fc0a1a7ae86e1c3
Calvin-C-M <montolalu28@gmail.com>
2021-11-07 21:29:09 +0700

    Init repo

---
+++ java/Soal-2/Main.java
@@ -0,0 +1,15 @@
+/*
+
+ANGGOTA KELOMPOK:
+- Naufal Fahrezi            (140810200035)
+- Amalia Nur Fitri          (140810200045)
+- Calvin Calfi Montolalu    (140810200053)
+- Deani Puteri Virdiana     (140810200059)
+
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ py/Soal-2/main.py
@@ -0,0 +1,5 @@
+# ANGGOTA KELOMPOK:
+# - Naufal Fahrezi            (140810200035)
+# - Amalia Nur Fitri          (140810200045)
+# - Calvin Calfi Montolalu    (140810200053)
+# - Deani Puteri Virdiana     (140810200059)
