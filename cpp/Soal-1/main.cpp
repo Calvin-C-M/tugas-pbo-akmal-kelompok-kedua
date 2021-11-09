b171c68f16f252b3fa89dc432bbacf6f88221bdb
amalianurf <amalia19006@mail.unpad.ac.id>
2021-11-09 22:01:10 +0700

    .

--- cpp/Soal-1/main.cpp
+++ cpp/Soal-1/main.cpp
@@ -17,15 +17,15 @@ int main() {
     Student *mahasiswa1=new Student();
     Student *mahasiswa2=new Student();
 
-    cout << "MAHASISWA 1" << endl;
+    cout << "===== MAHASISWA 1 =====" << endl;
     mahasiswa1->input();
 
-    cout << "MAHASISWA 2" << endl;
+    cout << "===== MAHASISWA 2 =====" << endl;
     mahasiswa2->input();
 
-    cout << "MAHASISWA 1" << endl;
+    cout << "===== MAHASISWA 1 =====" << endl;
     mahasiswa1->print();
 
-    cout << "MAHASISWA 2" << endl;
+    cout << "===== MAHASISWA 2 =====" << endl;
     mahasiswa2->print();
 }
