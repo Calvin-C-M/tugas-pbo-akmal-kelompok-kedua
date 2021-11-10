091d399118dee5e1977f4210101a699d80f4d0e4
Calvin-C-M <montolalu28@gmail.com>
2021-11-10 21:02:35 +0700

    Added main soal-1 java

--- java/Soal-1/Main.java
+++ java/Soal-1/Main.java
@@ -10,6 +10,19 @@ ANGGOTA KELOMPOK:
 
 public class Main {
     public static void main(String[] args) {
-        
+        Student mahasiswa1=new Student();
+        Student mahasiswa2=new Student();
+
+        System.out.println("MAHASISWA 1");
+        mahasiswa1.input();
+
+        System.out.println("MAHASISWA 2");
+        mahasiswa2.input();
+
+        System.out.println("MAHASISWA 1");
+        mahasiswa1.print();
+
+        System.out.println("MAHASISWA 2");
+        mahasiswa2.print();
     }
 }
